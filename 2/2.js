508c638982221c6936568a259996637d348d19d6
Arie3s <104190110+Arie3s@users.noreply.github.com>
2023-04-11 16:13:43 +0500

    Update 2.js

--- 2/2.js
+++ 2/2.js
@@ -1,13 +1,12 @@
-function find(key,arr,i=0){
-    var j=i++;
-    var arr2=arr.slice(i);
+function find(key,arr){
+    var arr2=arr.slice(1);
     if(arr[0]===key && arr.length != 0){
         return true;
     }else if(arr.length===0){return false;}
-    return find(key,arr2,j++);
+    return find(key,arr2);
 }
 
 
 
 var arr = [1,6,3,5,8,2,0];
-console.log(find(9,arr));
+console.log(find(23,arr));
